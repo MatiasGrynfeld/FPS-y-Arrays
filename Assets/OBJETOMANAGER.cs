b678680dc6f407e245355bb3b7471817ec2f7343
MatiasGrynfeld <141443983+MatiasGrynfeld@users.noreply.github.com>
2024-06-10 10:45:14 -0300

    Agregado tp 9, falta terminar

---
+++ Assets/OBJETOMANAGER.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OBJETOMANAGER : MonoBehaviour
+{
+    public int index = 0;
+    public objetoBehaviour[] objetos;
+    void Start()
+    {
+        deactivateAll();
+        InvokeRepeating(nameof(activateNext), 1, 3);
+    }
+    public void deactivateAll()
+    {
+        foreach (objetoBehaviour objeto in objetos)
+        {
+            objeto.gameObject.SetActive(false);
+        }
+    }
+
+    public void activateNext()
+    {
+        deactivateAll();
+        objetos[index].gameObject.SetActive(true);
+        index++;
+        if (index == objetos.Length)
+        {
+            index = 0;
+        }
+    }
+}
